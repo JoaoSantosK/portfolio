d1fe78f387650e0aee18169ae8faa578651a6a5e
João Santos <navijcsantos@gmail.com>
2026-04-13 14:13:24 -0300

    Header component
    Componente do Header desenvolvido, falta alterações básicas e desenvolver a parte responsiva.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import Header from "./components/Header/Header.jsx";
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Header />
   )
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>João Santos</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
