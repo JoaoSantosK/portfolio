373f04b4bba545c051ee9275404486794732f2d5
João Santos <navijcsantos@gmail.com>
2026-04-14 11:11:19 -0300

    Responsive header
    Desenvolvimento do responsivo para o header, com links, usando fontawesome (integrado via cdn) e ativado a partir de uma media screen igual ou inferior a 768px, lista com itens desce assim que o icone (bars) for clicado, o mesmo (quando a barra de navegação esta ativada) muda par ao icone de X, que quando clicado fecha o menu de navegação, funcionando nos mais variados tipos de tela.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,31 @@
 import './Header.css';
+import { useState} from 'react';
 
 const Header = () => {
+    const [isNavActive, setIsNavActive] = useState(false);
+
+    const toggleNav = () => {
+        setIsNavActive(!isNavActive);
+    };
+
     return (
         <header>
             <h1>João Santos</h1>
-            <nav>
-                <ul>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Projects</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
-            </nav>
+                <nav className="nav-desk">
+                    <ul>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Projects</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
+                <nav className="nav-mob">
+                    <i className={isNavActive ? 'fa-solid fa-xmark icon' : 'fa-solid fa-bars icon'} id='menu-icon' onClick={toggleNav}></i>
+                    <ul className={isNavActive ? 'active' : ''}>
+                        <li onClick={toggleNav}><a href="#">About</a></li>
+                        <li onClick={toggleNav}><a href="#">Projects</a></li>
+                        <li onClick={toggleNav}><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
         </header>
     )
 }
