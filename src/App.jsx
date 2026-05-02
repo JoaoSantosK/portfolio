2bd4116ba70542345211345c33bd7baf3822aa3a
João Santos <navijcsantos@gmail.com>
2026-05-02 03:43:10 -0300

    TalkToMe Section
    Criado protótipo da seção de "fale comigo".

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import MainContent from "./components/MainContent/MainContent";
 import WorkSection from "./components/WorkSection/WorkSection";
 import TechSection from "./components/TechSection/TechSection";
 import Footer from "./components/Footer/Footer";
+import TalkToMe from "./components/TalkToMe/TalkToMe";
 
 function App() {
   return (
---
+++ src/components/TalkToMe/TalkToMe.jsx
@@ -0,0 +1,15 @@
+import './TalkToMe.css';
+
+const TalkToMe = () => {
+    return (
+        <section className="talkTM">
+            <div className="title-line">
+                <div className="bar-h"></div>
+                <h1>Talk to me</h1>
+                <div className="bar-h"></div>
+            </div>
+        </section>
+    )
+}
+
+export default TalkToMe;
