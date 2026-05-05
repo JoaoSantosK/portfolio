2177b8911268843226e601dde42f4bcd0e1e3704
João Santos <navijcsantos@gmail.com>
2026-05-05 14:56:10 -0300

    Add content
    Adição de conteúdo para interação/direcionamento entre páginas no Footer. Adição talvez não permanente.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,11 @@ const Footer = () => {
     return (
         <footer className='footer-contato'>
             <p>© 2026 Kaizen. Todos os direitos reservados.</p>
+            <div className="contato">
+                <a href="#"><i className="fab fa-github"></i></a>
+                <a href="#"><i className="fab fa-linkedin-in"></i></a>
+                <a href="#"><i className="fab fa-instagram"></i></a>
+            </div>
         </footer>
     )
 }
