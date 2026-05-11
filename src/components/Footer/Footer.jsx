6c1ec43ef80fe4925262e3146d6955e1dea51042
João Santos <navijcsantos@gmail.com>
2026-05-11 16:03:40 -0300

    Links to social media
    Adicionado os links para redes sociais

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,9 +5,9 @@ const Footer = () => {
         <footer className='footer-contato'>
             <p>© 2026 Kaizen. Todos os direitos reservados.</p>
             <div className="contato">
-                <a href="#"><i className="fab fa-github"></i></a>
-                <a href="#"><i className="fab fa-linkedin-in"></i></a>
-                <a href="#"><i className="fab fa-instagram"></i></a>
+                <a href="https://github.com/JoaoSantosK?tab=overview&from=2026-05-01&to=2026-05-11" target='_blank'><i className="fab fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/jo%C3%A3o-santos-2409h/" target='_blank'><i className="fab fa-linkedin-in"></i></a>
+                <a href="https://www.instagram.com/kaizensl.dev/" target='_blank'><i className="fab fa-instagram"></i></a>
             </div>
         </footer>
     )
