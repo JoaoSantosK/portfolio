a632becb6e39344b2fef829e7444f2713e8345d1
João Santos <navijcsantos@gmail.com>
2026-05-25 22:12:37 -0300

    Área de projetos desenvolvida
    Área de projetos desenvolvida, agora com mais projetos e descrições

--- src/pages/MyWork/components/MyWorkMain/MyWorkMain.jsx
+++ src/pages/MyWork/components/MyWorkMain/MyWorkMain.jsx
@@ -1,7 +1,12 @@
 import './MyWorkMain.css';
 import ProjectCard from '../ProjectCard/ProjectCard';
 
-import project01DI from '../../../../assets/api-js.png';
+import project01DI from '../../../../assets/projects/api-js.png';
+import project02DI from '../../../../assets/projects/cardapio-delivery-pj.png';
+import project03DI from '../../../../assets/projects/java-program-project.png';
+
+import project04DI from '../../../../assets/projects/agencia-marketing-project.png';
+import project04M from '../../../../assets/projects/agencia-marketing-projectM.png';
 
 const MyWorkMain = () => {
     return(
@@ -27,25 +32,64 @@ const MyWorkMain = () => {
                     link="https://github.com/JoaoSantosK/api-login"
                 />
                 <ProjectCard
-                    title="Project Title"
+                    title="Cardápio Digital"
                     description={
                         <>
-                            <p>Outro projeto pessoal desenvolvido com React e Node.js</p>
+                            <p>
+                                Projeto de cardápio digital desenvolvido com JavaScript. Permite aos usuários navegar pelo cardápio, adicionar itens ao carrinho e realizar pedidos. O backend foi desenvolvido com Express.js e MongoDB, utilizando Prisma para gerenciar os dados.
+                            </p>
+                            <p>
+                                Atualizações futuras: Aperfeiçoar front-end; Adição de funcionalidades como personalização de pedidos; Sistema de pagamento online; Sistema de avaliação e feedback dos clientes; Gerenciamento de estoque para os administradores;
+                            </p>
+                            <div className="project-techs">
+                                <i class="fa-brands fa-html5"></i>
+                                <i class="fa-brands fa-css3-alt"></i>
+                                <i className="fa-brands fa-js"></i>
+                            </div>
                         </>
-                    }
-                    desktopImg={"https://placehold.co/500x300"}
-                    mobileImg={"https://placehold.co/200x300"}
-                    link="https://example.com/project-link"
+                    }   
+                    desktopImg={project02DI}
+                    mobileImg={null}
+                    link="https://github.com/JoaoSantosK/cardapio-delivery"
                 />
                 <ProjectCard
-                    title="Project Title"
+                    title="Programa de prioridade"
                     description={
                         <>
-                            <p>Terceiro projeto pessoal desenvolvido com React e Node.js</p>
+                            <p>
+                                Projeto desenvolvido com Java para gerenciamento de prioridades. Sistema tem como objetivo organizar e priorizar famílias com base em critérios pré-definidos.
+                            </p>
+                            <p>
+                                Atualizações futuras: Implementar funcionalidades adicionais, como geração de relatórios e gráficos para análise de dados; Adicionar autenticação e controle de acesso para diferentes níveis de usuários; Melhorar a interface do usuário para facilitar a navegação e a usabilidade do sistema;
+                            </p>
+                            <div className="project-techs">
+                                <i class="fa-brands fa-java"></i>
+                                <i class="fa-brands fa-openai"></i>
+                            </div>
                         </>
                     }
-                    desktopImg={"https://placehold.co/500x300"}
-                    mobileImg={"https://placehold.co/200x300"}
+                    desktopImg={project03DI}
+                    mobileImg={null}
+                    link="https://github.com/JoaoSantosK/programa-prioridade"
+                />
+                <ProjectCard
+                    title="Agência de Marketing Digital"
+                    description={
+                        <>
+                            <p>
+                                Projeto de agência de marketing digital desenvolvido com React. O projeto inclui uma landing page responsiva, destacando os serviços oferecidos pela agência, como SEO, gestão de redes sociais e criação de conteúdo. O design é moderno e atraente, visando captar a atenção dos visitantes e convertê-los em clientes.
+                            </p>
+                            <p>
+                                Atualizações futuras: Adicionar mais seções à landing page, como depoimentos de clientes e um portfólio de trabalhos anteriores; Implementar um formulário de contato funcional para facilitar a comunicação com os visitantes; Otimizar o desempenho do site para garantir uma experiência de usuário fluida e rápida;
+                            </p>
+                            <div className="project-techs">
+                                <i class="fa-brands fa-html5"></i>
+                                <i class="fa-brands fa-css3-alt"></i>
+                            </div>
+                        </>
+                    }
+                    desktopImg={project04DI}
+                    mobileImg={project04M}
                     link="https://example.com/project-link"
                 />
             </section>
