24bab35cb0819403bdee71c1b4ebafc6eb60357b
João Santos <navijcsantos@gmail.com>
2026-06-08 17:45:52 -0300

    Added scrolltotop
    ADicionado componente que sobe a página ao realizar uma troca entre páginas

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home/Home";
 import MyWork from "./pages/MyWork/MyWork";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter basename="/portfolio">
+      <ScrollToTop />
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/mywork" element={<MyWork />} />
