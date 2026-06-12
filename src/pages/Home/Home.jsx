25e8e1e5f2394e08ae0f28b0edbbbf8b9a9e2504
João Santos <navijcsantos@gmail.com>
2026-06-12 15:16:02 -0300

    background blur fixed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,16 +11,20 @@ import Footer from "../../components/Footer/Footer";
 
 function Home() {
   const [isContactOpen, setIsContactOpen] = useState(false);
+  
   return (
     <>
-    <Header />
-    <MainContent onOpenContact={() => setIsContactOpen(true)} />
+    <div className={isContactOpen ? "page-content blurred" : "page-content"}>
+      <Header />
+      <MainContent onOpenContact={() => setIsContactOpen(true)} />
+      <WorkSection />
+      <TechSection />
+      <Footer />
+    </div>
+
     {isContactOpen && (
       <ContactForm onClose={() => setIsContactOpen(false)} />
     )}
-    <WorkSection />
-    <TechSection />
-    <Footer />
     </>
   )
 }
